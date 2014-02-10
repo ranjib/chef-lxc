5147f3a5b79f5d644c3ecd6d6583c43a2df4609c
Ranjib Dey <ranjib@pagerduty.com>
2014-02-10 02:08:17 -0800

    stream only when tty is used

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ module LXCSpecHelper
     c.destroy if c.defined?
     recipe_file = File.expand_path('../data/' + recipe, __FILE__)
     command = Mixlib::ShellOut.new('chef-apply '+ recipe_file, timeout: 20*60)
-    command.live_stream=$stdout
+    command.live_stream=$stdout if STDIN.tty?
     command.run_command
     command
   end
