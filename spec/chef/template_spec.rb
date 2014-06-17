bb6dd05327a9582d34c059f32fe4d4753add1f16
Ranjib Dey <ranjib@pagerduty.com>
2014-06-16 21:17:58 -0700

    rspec 3 fix. destroy container after each specs

--- spec/chef/template_spec.rb
+++ spec/chef/template_spec.rb
@@ -10,19 +10,23 @@ describe 'Chef lxc resource/provider' do
     execute_recipe('template.rb')
   end
 
+  after(:all) do
+    destroy_ct
+  end
+
   let(:ct) do
     LXC::Container.new('chef')
   end
 
   it 'container should be created' do
-    expect(ct.defined?).to be_true
+    expect(ct.defined?).to be(true)
   end
 
   it 'container should be running' do
-    expect(ct.running?).to be_true
+    expect(ct.running?).to be(true)
   end
 
-  it 'container should be ubuntu 10.04' do
+  it 'container should be ubuntu 14.04' do
     version = ct.execute do
         `lsb_release -a`
       end
